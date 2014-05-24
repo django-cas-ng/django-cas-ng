e209a37c760e2d124bebd7540d06a2c9132c82f3
Ming Chen <mockey.chen@gmail.com>
2014-05-24 09:13:40 -0700

    Get user model from get_user_model for Django 1.5+, old version of Django still use build-in User model.

--- django_cas_ng/models.py
+++ django_cas_ng/models.py
@@ -1,2 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+
