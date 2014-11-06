b5d0a173ca555f56d1162247e43021f2d52bbc81
Ming Chen <ming.c@live.com>
2014-11-06 11:00:01 -0800

    Add python 3.4 in testenv

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,9 +1,10 @@
+from django_cas_ng.models import *
+from django_cas_ng.backends import *
+from django_cas_ng.middleware import *
+from django_cas_ng.views import *
+from django_cas_ng.decorators import *
+
 def test_nothing_is_on_fire():
-    """
-    Simple smoke test to see that all of the modules load.
-    """
-    from django_cas_ng.models import *
-    from django_cas_ng.backends import *
-    from django_cas_ng.middleware import *
-    from django_cas_ng.views import *
-    from django_cas_ng.decorators import *
+    # Nothing to do here, this file is used for testing import works.
+    pass
+
