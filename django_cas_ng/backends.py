4e15fa83e423c08867b7e5e549dcc74ddbe6270d
dyeray <dyeray@gmail.com>
2015-02-21 00:35:54 +0000

    Fix: allow to fill attributes variable in cas2saml

--- django_cas_ng/backends.py
+++ django_cas_ng/backends.py
@@ -204,14 +204,14 @@ def _verify_cas2_saml(ticket, service):
                 if 'uid' in list(at.attrib.values()):
                     user = at.find(SAML_1_0_ASSERTION_NS + 'AttributeValue').text
                     attributes['uid'] = user
-                    values = at.findall(SAML_1_0_ASSERTION_NS + 'AttributeValue')
-                    if len(values) > 1:
-                        values_array = []
-                        for v in values:
-                            values_array.append(v.text)
-                            attributes[at.attrib['AttributeName']] = values_array
-                    else:
-                        attributes[at.attrib['AttributeName']] = values[0].text
+                values = at.findall(SAML_1_0_ASSERTION_NS + 'AttributeValue')
+                if len(values) > 1:
+                    values_array = []
+                    for v in values:
+                        values_array.append(v.text)
+                        attributes[at.attrib['AttributeName']] = values_array
+                else:
+                    attributes[at.attrib['AttributeName']] = values[0].text
         return user, attributes
     finally:
         page.close()
