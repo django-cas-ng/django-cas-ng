8d4d908f99b9ff87e696481d2a483b68c4007c83
Bryce Groff <brycegroff@gmail.com>
2015-10-29 13:46:46 -0700

    Add a backend test to make sure that if CAS_CREATE_USER is set to False that no new user is created.

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -41,6 +41,39 @@ def test_backend_authentication_creating_a_user(monkeypatch, django_user_model):
     ).exists()
 
 
+def test_backend_authentication_do_not_create_user(monkeypatch, django_user_model, settings):
+    """
+    Test the case where CAS authentication is creating a new user.
+    """
+    factory = RequestFactory()
+    request = factory.get('/login/')
+    request.session = {}
+
+    def mock_verify(ticket, service):
+        return 'test@example.com', {'ticket': ticket, 'service': service}, None
+
+    # we mock out the verify method so that we can bypass the external http
+    # calls needed for real authentication since we are testing the logic
+    # around authentication.
+    monkeypatch.setattr('django_cas_ng.cas.CASClientV2.verify_ticket', mock_verify)
+
+    # sanity check
+    assert not django_user_model.objects.filter(
+        username='test@example.com',
+    ).exists()
+
+    settings.CAS_CREATE_USER = False
+    backend = backends.CASBackend()
+    user = backend.authenticate(
+        ticket='fake-ticket', service='fake-service', request=request,
+    )
+
+    assert user is None
+    assert not django_user_model.objects.filter(
+        username='test@example.com',
+    ).exists()
+
+
 @pytest.mark.django_db
 def test_backend_for_existing_user(monkeypatch, django_user_model):
     """
