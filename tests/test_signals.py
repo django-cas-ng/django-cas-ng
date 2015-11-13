fab7b6dd9c3dde32364213b443b9624ef303790c
Valentin Samir <valentin.samir@crans.org>
2015-11-13 23:03:56 +0100

    Add some tests for logout signal

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,11 +1,103 @@
 from __future__ import absolute_import
 import pytest
 
+from django.conf import settings
 from django.test import RequestFactory
 from django.dispatch import receiver
+from importlib import import_module
 
+from django_cas_ng.models import SessionTicket
 from django_cas_ng.backends import CASBackend
-from django_cas_ng.signals import cas_user_authenticated
+from django_cas_ng.signals import cas_user_authenticated, cas_user_logout
+from django_cas_ng.views import login, logout
+
+
+SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+
+
+@pytest.mark.django_db
+def test_signal_when_user_logout_manual(monkeypatch, django_user_model):
+    session = SessionStore()
+    session['fake_session_key'] = 'fake-session_value'
+    session.save()
+    assert SessionStore(session_key=session.session_key) is not None
+
+    factory = RequestFactory()
+    request = factory.get('/logout')
+    request.session = session
+
+    # Create a fake session ticket and make sure it exists in the db
+    session_ticket = SessionTicket.objects.create(
+        session_key=session.session_key,
+        ticket='fake-ticket'
+    )
+
+    user = django_user_model.objects.create_user('test@example.com', '')
+    assert user is not None
+    request.user = user
+
+    callback_values = {}
+
+    @receiver(cas_user_logout)
+    def callback(sender, session, **kwargs):
+        callback_values.update(kwargs)
+        callback_values['session'] = dict(session)
+
+    response = logout(request)
+    assert request.user.is_anonymous() is True
+    assert 'user' in callback_values
+    assert callback_values['user'] == user
+    assert 'session' in callback_values
+    assert callback_values['session'].get('fake_session_key') == 'fake-session_value'
+    assert 'ticket' in callback_values
+    assert callback_values['ticket'] == 'fake-ticket'
+
+
+@pytest.mark.django_db
+def test_signal_when_user_logout_slo(monkeypatch, django_user_model, settings):
+    data = {'logoutRequest': '<samlp:LogoutRequest '
+                             'xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol">'
+                             '<samlp:SessionIndex>fake-ticket'
+                             '</samlp:SessionIndex></samlp:LogoutRequest>'
+           }
+
+    settings.CAS_VERSION = 'CAS_2_SAML_1_0'
+
+    factory = RequestFactory()
+    request = factory.post('/login', data)
+    # user session and current requests.session are different
+    request.session = {}
+
+    user = django_user_model.objects.create_user('test@example.com', '')
+    assert user is not None
+
+
+    session = SessionStore()
+    session['fake_session_key'] = 'fake-session_value'
+    session.save()
+    assert SessionStore(session_key=session.session_key) is not None
+
+    # Create a fake session ticket and make sure it exists in the db
+    session_ticket = SessionTicket.objects.create(
+        session_key=session.session_key,
+        ticket='fake-ticket'
+    )
+
+    callback_values = {}
+
+    @receiver(cas_user_logout)
+    def callback(sender, session, **kwargs):
+        callback_values.update(kwargs)
+        callback_values['session'] = dict(session)
+
+
+    response = login(request)
+    assert 'user' in callback_values
+    assert 'session' in callback_values
+    assert callback_values['session'].get('fake_session_key') == 'fake-session_value'
+    assert 'ticket' in callback_values
+    assert callback_values['ticket'] == 'fake-ticket'
+
 
 
 @pytest.mark.django_db
