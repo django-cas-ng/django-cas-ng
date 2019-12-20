44419b2f8912dfb8083224f5cf0d30e0e7fe926e
Jon Dufresne <jon.dufresne@gmail.com>
2019-12-20 13:58:33 -0800

    Replace deprecated ugettext_lazy with gettext_lazy
    On Python 3, they are equivalent. Fixes deprecation warnings.

--- django_cas_ng/__init__.py
+++ django_cas_ng/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 __all__ = []
 
--- django_cas_ng/middleware.py
+++ django_cas_ng/middleware.py
@@ -11,7 +11,7 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .views import LoginView as cas_login, LogoutView as cas_logout
 
